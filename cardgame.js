057d87c388e2d2363562584979537348e1409911
ilya0 <iosovets@gmail.com>
2017-02-26 10:15:52 -0800

    more rules

--- cardgame.js
+++ cardgame.js
@@ -2,3 +2,36 @@
 // need to create a repository of in game cards
 // two arrays to hold the cards hands
 // rating of how the cards play against each other
+
+
+//Rules
+//The ranking of the cards from highest to lowest are: 2 A K Q J 10 9 8 7 6 5 4 3.
+//The cards are also ranked based on their suits.
+//The ranking from highest to lowest are: Hearts, Diamonds, Clubs, Spades.
+
+
+// starting game
+//The person who holds the lowest card will begin the inaugural game.
+//The winner of the previous game will begin the following game.
+
+
+// playing game
+// Valid card plays are; Singles, Pairs (two cards of same value), Trips (3 cards of same value), Runs (3-12 consecutive cards), Chops, and Bombs.
+// Players must follow the trend of play i.e. if a pair is played the subsequent player(s) may not play trips; they must play a higher pair.
+// If a player plays a 3 card run the subsequent player must play a higher 3 card run. They may not play a 4 card run.
+// If a player is unable, or unwilling, to beat the previous play they may pass their turn for that round.
+// A round is considered complete when all players have had the option to play
+
+// valid card plays
+
+// single - 2d
+// pair of the same value - 2d,2k
+//  tripple - 2d,2k,2h
+// runs 3-12 consecutive 2d,3k,4h,5d,6k
+
+
+
+
+
+
+
