6c60ce23e5568f8fd10fdb7644ebb54d0c58ea39
ilya o <iosovets@gmail.com>
2017-02-27 11:41:55 -0800

    added some more functions for evaluating game play

--- cardgame.js
+++ cardgame.js
@@ -37,6 +37,7 @@ var deck = ["2h","ah","kh","qh","jh","10h","9h","8h","7h","6h","5h","4h","3h",
             "2s","as","ks","qs","js","10s","9s","8s","7s","6s","5s","4s","3s",
 
 ];
+//temp save deck so I dont have to retype trying out the combinations
 // var deck = ["2h","ah","kh","qh","jh","10h","9h","8h","7h","6h","5h","4h","3h",
 //             "2d","ad","kd","qd","jd","10d","9d","8d","7d","6d","5d","4d","3d",
 //             "2c","ac","kc","qc","jc","10c","9c","8c","7c","6c","5c","4c","3c",
@@ -59,24 +60,6 @@ var p2action = [];
 
 var begingame = function(){
 };
-
-//The winner of the previous game will begin the following game.
-
-
-// playing game
-// Valid card plays are; Singles, Pairs (two cards of same value), Trips (3 cards of same value), Runs (3-12 consecutive cards), Chops, and Bombs.
-// Players must follow the trend of play i.e. if a pair is played the subsequent player(s) may not play trips; they must play a higher pair.
-// If a player plays a 3 card run the subsequent player must play a higher 3 card run. They may not play a 4 card run.
-// If a player is unable, or unwilling, to beat the previous play they may pass their turn for that round.
-// A round is considered complete when all players have had the option to play
-
-// valid card plays
-
-// single - 2d
-// pair of the same value - 2d,2k
-//  tripple - 2d,2k,2h
-// runs 3-12 consecutive 2d,3k,4h,5d,6k
-
 
 
 
@@ -116,6 +99,8 @@ var begingame = function(){
     }
   }
 
+
+//The winner of the previous game will begin the following game.
 
   function playercheck(){
     // checks for the player who goes first
@@ -167,6 +152,48 @@ var begingame = function(){
 
   }
 }
+
+
+  function rungame(){
+    //stipulations
+    //cards shuffled and delt
+    // need to run playercheck
+    if(playercheck() === "player1"){
+      //initialize steps for player1 to select cards and play
+    }else{
+      //player2 goes first
+
+    }
+  }
+
+//push cards into action hand
+  function playercardturn(player){
+    //show cards
+    //ask to select cards to play
+    //push them into action slot
+  }
+
+// evaluate the turn
+  function evalturn(){
+    //evaluated the action hands
+  }
+
+
+// playing game
+// Valid card plays are; Singles, Pairs (two cards of same value), Trips (3 cards of same value), Runs (3-12 consecutive cards), Chops, and Bombs.
+// Players must follow the trend of play i.e. if a pair is played the subsequent player(s) may not play trips; they must play a higher pair.
+// If a player plays a 3 card run the subsequent player must play a higher 3 card run. They may not play a 4 card run.
+// If a player is unable, or unwilling, to beat the previous play they may pass their turn for that round.
+// A round is considered complete when all players have had the option to play
+
+// valid card plays
+
+// single - 2d
+// pair of the same value - 2d,2k
+//  tripple - 2d,2k,2h
+// runs 3-12 consecutive 2d,3k,4h,5d,6k
+
+
 
 
 
